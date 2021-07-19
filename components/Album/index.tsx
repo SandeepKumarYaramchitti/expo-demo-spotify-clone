8651efbf277ad3606055b17a2d78d6088bca19e2
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-07-19 17:07:47 -0400

    more updates to the home screen with Touchable Native feedback

--- components/Album/index.tsx
+++ components/Album/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react'
-import {View, Image, Text} from 'react-native'
+import {View, Image, Text, TouchableNativeFeedback} from 'react-native'
 import styles from './styles'
 import {Album} from '../../types'
 export type AlbumProps = {
     album: Album
 }
 const AlbumComponent = (props: AlbumProps) => {
+
+    const onPress = () => {
+        console.warn(`Album has been clicked ${props.album.artistsHeadline}`)
+    }
     return (
-        <View style={styles.container}>
-            <Image 
-                source={{ uri: props.album.imageUri}}
-                style={styles.image}
-            > 
-            </Image>
-            <Text style={styles.text}>{props.album.artistsHeadline}</Text>
-        </View>
+        <TouchableNativeFeedback onPress={onPress}>
+            <View style={styles.container}>
+                <Image 
+                    source={{ uri: props.album.imageUri}}
+                    style={styles.image}
+                > 
+                </Image>
+                <Text style={styles.text}>{props.album.artistsHeadline}</Text>
+            </View>
+        </TouchableNativeFeedback>
+
     )
 }
 
---
+++ screens/AlbumScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import {View, Text} from 'react-native';
+
+const AlbumScreen = () => (
+    <View>
+        <Text style={{color: 'Blue'}}>Hello, From Album screen</Text>
+    </View>
+)
+
+export default AlbumScreen
